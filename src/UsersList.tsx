9fe2a4752054adc19d53660d268bf89f8fc9fe42
Jeremy Shepherd <JeremyLShepherd@gmail.com>
2021-03-18 21:20:05 -0400

    Added UsersList component

---
+++ src/UsersList.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+
+interface Props {}
+
+type User = {
+    id: number,
+    firstName: string,
+    lastName: string,
+    email: string,
+    active: boolean
+};
+
+function UsersList(props: Props) {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const response = await fetch('https://exercises.getsandbox.com/users');
+            const users = await response.json();
+            setUsers(users);
+        }
+        getUsers();
+    }, []);
+
+    return (        
+        <>
+            <h2>List of Users:</h2>
+            <ul style={{listStyle: "none"}}>
+                {users.map((user: User) => (
+                    <li key={user.id}>{`${user.lastName}, ${user.firstName}`}</li>)
+                )}
+            </ul>
+        </>
+    )
+}
+
+export default UsersList;
