027ad5a1ad41ad15300981ef5c64a9df9b3338e5
Jeremy Shepherd <JeremyLShepherd@gmail.com>
2021-03-24 13:10:31 -0400

    Refactored style into separte const object

--- src/Components/ErrorAlert.tsx
+++ src/Components/ErrorAlert.tsx
@@ -6,21 +6,24 @@ interface Props {
 
 function ErrorAlert(props: Props) {
     const { message } = props;
+    const errorStyle = {
+        display: 'block',
+        backgroundColor: 'pink',
+        marginTop: '1em',
+        padding: '2em 3em',
+        borderRadius: '25px',
+        border: '1px solid red',
+        width: '80vw',
+        color: 'red',
+        textAlign: 'center',
+        fontWeight: 600
+    } as const;             //Prevents property reassignment
 
     return (
         <>
             {console.error(message)}
             <span 
-                style={ {
-                    backgroundColor: 'pink',
-                    padding: '2em 3em',
-                    borderRadius: '25px',
-                    border: '1px solid red',
-                    width: '80vw',
-                    color: 'red',
-                    textAlign: 'center',
-                    fontWeight: 600
-                }}
+                style={errorStyle}
             >
                 {message}
             </span>
